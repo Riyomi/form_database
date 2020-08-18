343409aa49b748bec745aeb1d2322d5477c67faa
Riyomi <sz.richard98@gmail.com>
2020-08-18 19:46:02 +0200

    Added login screen and navigation between the two screens.

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,18 @@ void main() {
     home: Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: PageForm()
+        child: SignUpPage()
       )
     )
   ));
 }
 
-class PageForm extends StatefulWidget {
+class SignUpPage extends StatefulWidget {
   @override
-  _PageFormState createState() => _PageFormState();
+  _SignUpPageState createState() => _SignUpPageState();
 }
 
-class _PageFormState extends State<PageForm> {
+class _SignUpPageState extends State<SignUpPage> {
   final _formKey = GlobalKey<FormState>();
 
   var _autoValidate = false;
@@ -103,7 +103,7 @@ class _PageFormState extends State<PageForm> {
                 width: double.infinity,
                 child: FlatButton(
                     color: Colors.blue,
-                    child: Text('Submit', style: TextStyle(
+                    child: Text('Sign up', style: TextStyle(
                       color: Colors.white,
                       fontWeight: FontWeight.bold,
                     )),
@@ -159,7 +159,16 @@ class _PageFormState extends State<PageForm> {
                     decoration: TextDecoration.underline,
                   )),
                 onTap: () => {
-                  //TODO: add function that would redirect to the login page
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => new Scaffold(
+                        body: Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: LoginPage()
+                        )
+                    )
+                    ),
+                  )
                 }
             ),
           ],
@@ -174,4 +183,138 @@ bool validateStructure(String value){
       r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
   RegExp regExp = new RegExp(pattern);
   return regExp.hasMatch(value);
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  var _autoValidate = false;
+  var _user;
+  var _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(children: <Widget>[
+        Form (
+            autovalidate: _autoValidate,
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 20),
+                FlutterLogo(size: 100),
+                SizedBox(height: 20),
+                Text('Login',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.grey,
+                    )),
+                SizedBox(height: 20),
+                TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Username'
+                    ),
+                    onChanged: (value) {
+                      _user = value;
+                    },
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter your username';
+                      }
+                      return null;
+                    }
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password'
+                  ),
+                  onChanged: (value) {
+                    _password = value;
+                  },
+                  validator: (value) {
+                    if(value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  width: double.infinity,
+                  child: FlatButton(
+                      color: Colors.blue,
+                      child: Text('Login', style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      )),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          print('$_user:$_password');
+                          try {
+                            await insertUser(new User(_user, _password));
+                            Scaffold.of(context)
+                                .showSnackBar(SnackBar(
+                              backgroundColor: Colors.green,
+                              content: Text(
+                                  'Successfully submitted'),));
+                          } on DatabaseException catch (_) {
+                            print("Error");
+                            Scaffold.of(context)
+                                .showSnackBar(SnackBar(
+                              backgroundColor: Colors.redAccent,
+                              content: Text(
+                                  'A user with this name already exists.'),));
+                          }
+                        } else {
+                          Scaffold.of(context)
+                              .showSnackBar(SnackBar(
+                            backgroundColor: Colors.redAccent,
+                            content: Text('Problem submitting form'),));
+                          setState(() {
+                            _autoValidate = true;
+                          });
+                        }
+                      }
+                  ),
+                ),
+              ],
+            )
+        ),
+        SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Don\'t have an account? ',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                )),
+            InkWell(
+                child: Text('Sign up.',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      decoration: TextDecoration.underline,
+                    )),
+                onTap: () => {
+                  Navigator.pop(context)
+                }
+            ),
+          ],
+        ),
+      ],),
+    );
+  }
 }
